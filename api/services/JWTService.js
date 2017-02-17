fcda46e56c46e9a68b0f67c788c70a7059184c65
utkarsh <utkarshrai003@gmail.com>
2017-02-17 14:57:26 +0530

    Added JWTService with issue_token and verify_token methods

---
+++ api/services/JWTService.js
@@ -0,0 +1,29 @@
+// This service holds the functions for generating and verifying a JSON Web Token
+
+var jwt = require('jsonwebtoken'),
+var tokenSecret = "secretissecet"; // this must be in configuration
+
+// Generates a token from supplied payload
+module.exports.issue_token = function(payload) {
+  return jwt.sign(
+    payload,
+    tokenSecret, // Token Secret that we sign it with
+    {
+      expiresInMinutes : 180 // Token Expire time
+    }
+  );
+};
+
+// Verifies token on a request
+module.exports.verify_token = function(token, callback) {
+  return jwt.verify(
+    token, // The token to be verified
+    tokenSecret, // Same token we used to sign
+    {}, // No Option, for more see https://github.com/auth0/node-jsonwebtoken#jwtverifytoken-secretorpublickey-options-callback
+    function() {
+      {
+        message: "token is verified"
+      }
+    } //Pass errors or decoded token to callback
+  );
+};
