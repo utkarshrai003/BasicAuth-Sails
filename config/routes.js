e2c57f60c99b107306be5dfc46566bd094645d20
utkarsh <utkarshrai003@gmail.com>
2017-02-17 19:51:09 +0530

    Corrected typo in Route JS

--- config/routes.js
+++ config/routes.js
@@ -34,7 +34,8 @@ module.exports.routes = {
 
   '/': {
     view: 'homepage'
-  }
+  },
+  'POST /users/signup': 'UserController.signup'
 
   /***************************************************************************
   *                                                                          *
