77cb73bcf7d8a80058ab9a3d4c2c3cab763ce234
utkarsh <utkarshrai003@gmail.com>
2017-02-20 19:34:01 +0530

    Added a method to sign in a user with Jwt Token strategy

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -6,6 +6,33 @@
  */
 
 module.exports = {
-	
-};
 
+	// POST /sigin
+	signin: function(req, res) {
+		params = req.allParams();
+
+		if(!params.email || !params.password) {
+			return res.send(400, { error: "Both email and password are required to login"})
+		}
+
+		if (params.password !== params.confirmPassword) {
+      return res.json(401, {err: 'Password doesn\'t match, What a shame!'});
+    }
+
+		User.findOne({email: params.email}).exec(function(err, record) {
+			if(err || !record) {
+				return res.send(401, { error: "No such user" })
+			}
+
+			User.comparePassword(params.password, record, function(err, match) {
+				if(err) {
+					res.send(400, {error: "Email and Password do not match"})
+				}
+
+				if(match) {
+					res.send(200, {user: record, token: JWTService.issue_token(record)} )
+				}
+			});
+		});
+	}
+};
