cdf0df886df67ba9bf320897864c2c854603f5d3
utkarsh <utkarshrai003@gmail.com>
2017-02-20 18:05:22 +0530

    Refactored Users controller to sign up a user

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -8,20 +8,16 @@
 module.exports = {
 
 	signup: function(req, res) {
-		params = req.query;
+		params = req.allParams();
+
 		if (!params.email || !params.password) {
 			res.send(400, {error: 'Both email and password required to create an account.'});
 		}
 
-    User.create({
-			first_name: params.first_name,
-		  last_name: params.last_name,
-		  mail: params.email,
-		  password: params.password
-	  })
+    User.create(params)
 		.exec(function(err, record) {
 			if (err) { res.send(400, {error: err}) }
-			res.send(200, { data: record });
+		  res.send(200, {data: record});
 	 });
 	}
 
